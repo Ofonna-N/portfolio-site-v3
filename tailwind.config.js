dda77561849b4344ac61131a762d70d22d8ab170
Ofonna-N <nwezeofonna@gmail.com>
2023-06-02 11:55:18 -0500

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-50": "var(--primary-50)",
+        "primary-100": "var(--primary-100)",
+        "primary-200": "var(--primary-200)",
+        "primary-300": "var(--primary-300)",
+        "primary-400": "var(--primary-400)",
+        "primary-500": "var(--primary-500)",
+        "primary-600": "var(--primary-600)",
+        "primary-700": "var(--primary-700)",
+        "primary-800": "var(--primary-800)",
+        "primary-900": "var(--primary-900)",
       },
     },
   },
   plugins: [],
-}
+};
