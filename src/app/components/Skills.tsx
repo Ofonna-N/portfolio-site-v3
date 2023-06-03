a3aa6678a7614f3ba2b2b036aee4fe32fa6b7294
Ofonna-N <nwezeofonna@gmail.com>
2023-06-03 15:14:29 -0500

    Fixed Skill layout

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -38,13 +38,13 @@ const skills: SkillGroup[] = [
 
 const Skills = () => {
   return (
-    <div className="px-[2rem] h-full flex flex-col justify-center items-center">
+    <div className="px-[2rem] h-full flex flex-col justify-center items-center gap-[3rem]">
       <h3 className="text-center text-[3rem]">SKILLS</h3>
       <ul className="">
         {skills.map((skillGroup) => (
-          <>
+          <li>
             <h3 className="text-[2.4rem]">{skillGroup.title}</h3>
-            <hr className="mb-[2rem]" />
+            <hr className="mb-[1rem]" />
             <ul className="grid grid-cols-3 gap-3 mb-[2rem]">
               {skillGroup.skillSets.map((skill) => (
                 <li className="">
@@ -52,7 +52,7 @@ const Skills = () => {
                 </li>
               ))}
             </ul>
-          </>
+          </li>
         ))}
       </ul>
     </div>
