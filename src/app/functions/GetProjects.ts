bff91c8259127f53bf0fb33f8a19f9ba47ff7b85
Ofonna-N <nwezeofonna@gmail.com>
2023-06-10 12:53:06 -0500

    update - returned fetched projects without creating constant

--- src/app/functions/GetProjects.ts
+++ src/app/functions/GetProjects.ts
@@ -2,9 +2,7 @@ import sanityClient from "@/services/sanityClient";
 import ProjectType from "../customTypes/ProjectType";
 
 export default async function GetProjects(): Promise<ProjectType[]> {
-  const projects = await sanityClient.fetch(
+  return await sanityClient.fetch(
     '*[_type == "project"]{..., "imgSrc":image.asset->url}'
   );
-
-  return projects;
 }
