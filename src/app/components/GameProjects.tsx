48e2b70f8967c411581a857164ac94ae81151b57
Ofonna-N <nwezeofonna@gmail.com>
2023-06-10 15:13:22 -0500

    edit - fixed layout for project sections

--- src/app/components/GameProjects.tsx
+++ src/app/components/GameProjects.tsx
@@ -7,16 +7,20 @@ const GameProjects = async () => {
   const projects = await GetGameProjects();
 
   return (
-    <div className="h-full px-[2rem] flex flex-col items-center justify-center gap-[1rem] md:gap-[4rem]">
-      <h3 className="text-[3rem] md:text-[5rem] text-center">GAME PROJECTS</h3>
-      <div className="relative w-full ">
-        <ImageGallery>
-          {projects.map((project, i) => (
-            <GalleryItem className="w-full h-fit md:w-1/2" key={i}>
-              <ProjectCard project={project} />
-            </GalleryItem>
-          ))}
-        </ImageGallery>
+    <div className="relative h-full px-[2rem] flex flex-col items-center justify-center gap-[1rem] md:gap-[4rem]">
+      <div className="h-[80%] relative w-full flex flex-col items-center justify-center">
+        <h3 className="text-[3rem] md:text-[5rem] text-center">
+          GAME PROJECTS
+        </h3>
+        <div className="relative w-full ">
+          <ImageGallery>
+            {projects.map((project, i) => (
+              <GalleryItem className="w-full h-fit md:w-1/2" key={i}>
+                <ProjectCard project={project} />
+              </GalleryItem>
+            ))}
+          </ImageGallery>
+        </div>
       </div>
     </div>
   );
--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -9,15 +9,17 @@ const Project = async () => {
 
   return (
     <div className="h-full px-[2rem] flex flex-col items-center justify-center gap-[1rem] md:gap-[4rem]">
-      <h3 className="text-[3rem] md:text-[5rem] text-center">WEB PROJECTS</h3>
-      <div className="relative w-full ">
-        <ImageGallery>
-          {projects.map((project, i) => (
-            <GalleryItem className="w-full h-fit md:w-1/2" key={i}>
-              <ProjectCard project={project} />
-            </GalleryItem>
-          ))}
-        </ImageGallery>
+      <div className="relative w-full h-4/5 flex flex-col justify-center items-center">
+        <h3 className="text-[3rem] md:text-[5rem] text-center">WEB PROJECTS</h3>
+        <div className="relative w-full ">
+          <ImageGallery>
+            {projects.map((project, i) => (
+              <GalleryItem className="w-full h-fit md:w-1/2" key={i}>
+                <ProjectCard project={project} />
+              </GalleryItem>
+            ))}
+          </ImageGallery>
+        </div>
       </div>
     </div>
   );
